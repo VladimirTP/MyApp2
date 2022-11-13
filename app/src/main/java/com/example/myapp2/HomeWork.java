12ee4596c78446eb5bcd4f17608e3307d86d7f47
TamashovVladimir <v.tamashov@gmail.com>
2022-11-13 15:07:03 +0300

    task1

--- app/src/main/java/com/example/myapp2/HomeWork.java
+++ app/src/main/java/com/example/myapp2/HomeWork.java
@@ -1,8 +1,23 @@
 package com.example.myapp2;
 
+import java.util.Scanner;
+
 public class HomeWork {
     public static void main (String [] args) {
-
+        task1 ();
 
     }
+
+    static void task1 () {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = in.nextInt();
+
+        if (a % 2 == 0) {
+            System.out.println("Число четное");
+        } else {
+            System.out.println("Число нечетное");
+        }
+    }
+
 }
