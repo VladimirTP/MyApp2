48fe15c6b265b087f35b4bb446413a49b92af673
TamashovVladimir <v.tamashov@gmail.com>
2022-11-13 15:34:36 +0300

    task4

--- app/src/main/java/com/example/myapp2/HomeWork.java
+++ app/src/main/java/com/example/myapp2/HomeWork.java
@@ -2,7 +2,15 @@ package com.example.myapp2;
 
 public class HomeWork {
     public static void main (String [] args) {
+        task4 ();
 
+
+    }
+
+    static void task4 () {
+         for (int i = 0; i<=100; i+=5) {
+             System.out.println(i);
+         }
 
     }
 }
